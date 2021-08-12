91650f79008bd7574a0081945ee794d013503700
Ar. Tareq <kanizety789@gmail.com>
2021-08-13 03:27:00 +0600

    converted image signature from BGR to RGB

--- camera.cpp
+++ camera.cpp
@@ -4,66 +4,65 @@
 #include <chrono>
 #include <ctime>
 
+
 using namespace std;
 using namespace cv;
 using namespace raspicam;
 
 Mat frame;
+RaspiCam_Cv Camera;
 
 
-
- void Setup ( int argc,char **argv, RaspiCam_Cv &Camera )
-  {
-    Camera.set ( CAP_PROP_FRAME_WIDTH,  ( "-w",argc,argv,400 ) );
+void Setup ( int argc,char **argv, RaspiCam_Cv &Camera )
+{
+    Camera.set ( CAP_PROP_FRAME_WIDTH,  ( "-w",argc,argv,360 ) );
     Camera.set ( CAP_PROP_FRAME_HEIGHT,  ( "-h",argc,argv,240 ) );
     Camera.set ( CAP_PROP_BRIGHTNESS, ( "-br",argc,argv,50 ) );
     Camera.set ( CAP_PROP_CONTRAST ,( "-co",argc,argv,50 ) );
     Camera.set ( CAP_PROP_SATURATION,  ( "-sa",argc,argv,50 ) );
     Camera.set ( CAP_PROP_GAIN,  ( "-g",argc,argv ,50 ) );
-    Camera.set ( CAP_PROP_FPS,  ( "-fps",argc,argv,100));
+    Camera.set ( CAP_PROP_FPS,  ( "-fps",argc,argv,30));
 
 }
 
+void Capture()
+{
+    Camera.grab();
+    Camera.retrieve(frame);
+    cvtColor(frame, frame, COLOR_BGR2RGB);
+}
 
-
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
 	
-        RaspiCam_Cv Camera;
-	Setup(argc, argv, Camera);
+    Setup(argc, argv, Camera);
 	cout<<"Connecting to camera"<<endl;
-	if (!Camera.open())
+	if (!Camera.open())	
 	{
-		
-	cout<<"Failed to Connect"<<endl;
-     }
-     
-     cout<<"Camera Id = "<<Camera.getId()<<endl;
-     
-     
-     
+		cout<<"Failed to Connect"<<endl;
+		return -1;
+    }
+    cout<<"Camera Id = "<<Camera.getId()<<endl;
     
     while(1)
     {
-    auto start = std::chrono::system_clock::now();
-    Camera.grab();
-    Camera.retrieve( frame);
-    auto end = std::chrono::system_clock::now();
-  
-
-    std::chrono::duration<double> elapsed_seconds = end-start;
+	auto start = std::chrono::system_clock::now();
+	Capture();
+	
+	
+	namedWindow("FrontView", WINDOW_KEEPRATIO);
+	moveWindow("FrontView", 150, 100);
+	resizeWindow("FrontView", 640, 480);
+	imshow("FrontView", frame);
+	
+	waitKey(1);
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end-start;
+	float t = elapsed_seconds.count();
+	int FPS = 1/t;
+	cout<<"FPS = "<<FPS<<endl;
     
-    float t = elapsed_seconds.count();
-    int FPS = 1/t;
-    cout<<"FPS = "<<FPS<<endl;
-    imshow("orignal", frame);
-  
-    waitKey(1);
-    
-    
-    }
-
-    
-    return 0;
+	}
+	return 0;
      
-}
+    }
