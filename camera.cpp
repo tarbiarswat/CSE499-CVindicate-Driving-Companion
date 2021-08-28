cb72ec6b73c5c1cbc477e7fa5e670bed1e886d06
tarbiarswat <tarbiar.swat@northsouth.edu>
2021-08-28 11:33:36 -0900

    object dataset version 1 captured

--- camera.cpp
+++ camera.cpp
@@ -36,8 +36,8 @@ void Setup ( int argc,char **argv, RaspiCam_Cv &Camera )
     Camera.set ( CAP_PROP_FRAME_HEIGHT,  ( "-h",argc,argv,240 ) );
     Camera.set ( CAP_PROP_BRIGHTNESS, ( "-br",argc,argv,50 ) );
     Camera.set ( CAP_PROP_CONTRAST ,( "-co",argc,argv,50 ) );
-    Camera.set ( CAP_PROP_SATURATION,  ( "-sa",argc,argv,50 ) );
-    Camera.set ( CAP_PROP_GAIN,  ( "-g",argc,argv ,50 ) );
+    Camera.set ( CAP_PROP_SATURATION,  ( "-sa",argc,argv,70 ) );
+    Camera.set ( CAP_PROP_GAIN,  ( "-g",argc,argv ,150 ) );
     Camera.set ( CAP_PROP_FPS,  ( "-fps",argc,argv,0));
 
 }
